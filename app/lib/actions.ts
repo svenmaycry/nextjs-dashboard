38c57e9fdc722f36ff216cd786ce45683e7c843c
SvenMayCry <svenmaycry@gmail.com>
2024-07-03 10:55:28 +0300

    small fix.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -47,6 +47,7 @@ export async function createInvoice(prevState:State, formData: FormData) {
     };
   }
 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
